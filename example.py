ad12cd507fbfab959f41da7d9152f3ab97d8ce8d
Gurox <93458769+GuroxDG@users.noreply.github.com>
2023-12-15 14:02:31 -0500

    Update example.py

--- example.py
+++ example.py
@@ -41,4 +41,55 @@ print(stephansdom.tag('name'))
 print('%s %s, %s %s' % (stephansdom.tag('addr:street'), stephansdom.tag('addr:housenumber'), stephansdom.tag('addr:postcode'), stephansdom.tag('addr:city')))
 print(stephansdom.tag('building'))
 print(stephansdom.tag('denomination'))
+
+# %%
+
+# How many trees are in the OSM data of Vienna? And how many trees have there been in 2013?
+
+# This time, we have to first resolve the name "Vienna" to an area id:
+
+from OSMPythonTools.nominatim import Nominatim
+nominatim = Nominatim()
+nominatim.u
+areaId = nominatim.query('Vienna, Austria').areaId()
+# %%
+
+from OSMPythonTools.overpass import overpassQueryBuilder, Overpass
+overpass = Overpass()
+query = overpassQueryBuilder(area=areaId, elementType='node', selector='"natural"="tree"', out='count')
+result = overpass.query(query)
+result.countElements()
+# %%
+result = overpass.query(query, date='2013-01-01T00:00:00Z', timeout=60)
+result.countElements()
+
+
+# %%
+
+#How did the number of trees in Berlin, Paris, and Vienna change over time?
+#Before we can answer the question, we have to import some modules:
+
+from collections import OrderedDict
+from OSMPythonTools.data import Data, dictRangeYears, ALL
+from OSMPythonTools.overpass import overpassQueryBuilder, Overpass
+
+dimensions = OrderedDict([
+    ('year', dictRangeYears(2013, 2017.5, 1)),
+    ('city', OrderedDict({
+        'berlin': 'Berlin, Germany',
+        'paris': 'Paris, France',
+        'vienna': 'Vienna, Austria',
+    })),
+])
+
+overpass = Overpass()
+def fetch(year, city):
+    areaId = nominatim.query(city).areaId()
+    query = overpassQueryBuilder(area=areaId, elementType='node', selector='"natural"="tree"', out='count')
+    return overpass.query(query, date=year, timeout=60).countElements()
+data = Data(fetch, dimensions)
+
+data.plot(city=ALL, filename='example4.png')
+
+data.select(city=ALL).getCSV()
 # %%
